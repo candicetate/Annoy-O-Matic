e4100a192157f1e00f20d4c1e5fa93604caf6ac7
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-08-02 12:31:02 -0600

    added while loop

--- src/loops.js
+++ src/loops.js
@@ -1,5 +1,12 @@
-alert("connected");
-
 // Ask the user, are we there yet?
+
+var answer = prompt("Are we there yet?");
+
 // Keep asking until they say yes or yeah
+
+while (answer.indexOf("yes") === -1 && answer !== "yeah") {
+  var answer = prompt("Are we there yet?");
+}
+
 // Alert yay we made it
+alert("Woo hoo!");
